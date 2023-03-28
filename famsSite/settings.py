f548e0f8ae55f5ffe09c12b10e3a85279d53fa10
kensidiangco <kensidiangco@gmail.com>
2023-03-29 03:18:05 +0800

    Update settings

--- famsSite/settings.py
+++ famsSite/settings.py
@@ -13,10 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['fams-sjc.azurewebsites.net']
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_URLNAME']]
+ALLOWED_HOSTS = ['fams-sjc.azurewebsites.net', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -132,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
