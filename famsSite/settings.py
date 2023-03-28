bd76d21aaf2c3ed7acd710098a985f57c965a3e0
kensidiangco <kensidiangco@gmail.com>
2023-03-29 03:20:06 +0800

    Update settings

--- famsSite/settings.py
+++ famsSite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fams-sjc.azurewebsites.net', '127.0.0.1', 'localhost']
 
