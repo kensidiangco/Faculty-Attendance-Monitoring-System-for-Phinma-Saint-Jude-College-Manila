1c7942b27957da9ffe652f30714ec7635ede9d94
kensidiangco <kensidiangco@gmail.com>
2023-03-29 01:45:48 +0800

    Configure deployment to MS Azure

--- famsSite/settings.py
+++ famsSite/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'get_random_secret_key()'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['fams-sjc.azurewebsites.net']
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
