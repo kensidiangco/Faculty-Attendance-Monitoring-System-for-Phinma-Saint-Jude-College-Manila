ae1601b063b9638b7b2e61e0e1939771d93ffa85
kensidiangco <kensidiangco@gmail.com>
2023-03-29 02:55:38 +0800

    Update settings

--- famsSite/settings.py
+++ famsSite/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
+    BASE_DIR / 'static'
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
