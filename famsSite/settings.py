04c806df02e95db977f2bb4622ff43b8208a11ac
kensidiangco <kensidiangco@gmail.com>
2023-03-29 16:15:31 +0800

    Update settings

--- famsSite/settings.py
+++ famsSite/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
