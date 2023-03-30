01c48b36e50ec76de59c694cd0e1fe02b042cbe0
kensidiangco <kensidiangco@gmail.com>
2023-03-31 04:53:41 +0800

    Update app

--- famsSite/settings.py
+++ famsSite/settings.py
@@ -38,12 +38,6 @@ INSTALLED_APPS = [
     'django_browser_reload',
 ]
 
-# TAILWIND CONFIGURATION
-TAILWIND_APP_NAME = 'theme'
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     "django_browser_reload.middleware.BrowserReloadMiddleware",
@@ -84,8 +78,12 @@ WSGI_APPLICATION = 'famsSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ['DB_ENGINE'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PSSWRD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
