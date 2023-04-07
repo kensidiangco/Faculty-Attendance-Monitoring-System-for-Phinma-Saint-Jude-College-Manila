cce70efae45af6e20f1f1998312693db0462163a
kensidiangco <kensidiangco@gmail.com>
2023-04-07 19:49:47 +0800

    Updating apscheduler command

--- famsSite/apps/attendance/management/commands/runapscheduler.py
+++ famsSite/apps/attendance/management/commands/runapscheduler.py
@@ -14,22 +14,28 @@ logger = logging.getLogger(__name__)
 timezone = pytz.timezone('Asia/Manila')
 
 def update_sched_job():
+    print("running")
     done_scheds = Schedule.objects.filter(status="DONE")
     for sched in done_scheds:
         if str(sched.weekday).upper() == datetime.now().strftime('%A').upper():
             sched.status = "VACANT"
             sched.save()
-
+        else:
+            print("SCHEDULE ALREADY UPDATED...")
+            logger.info("SCHEDULE ALREADY UPDATED...")
     logger.info("SCHEDULE STATUS UPDATED...")
     
 def sched_time_out_tracker_job():
+    print("running")
     ongoing_dtrs = Employee_DTR.objects.filter(status="ONGOING")
     for dtr in ongoing_dtrs:
         if dtr.schedule.time_out == datetime.now().time():
             dtr.time_out = datetime.now(timezone)
             dtr.save()
-            
-    logger.info("DTR OUT UPDATED...")
+        else:
+            print("DTR TIME OUT ALREADY UPDATED...")
+            logger.info("DTR TIME OUT ALREADY UPDATED...")
+    logger.info("DTR TIME OUT UPDATED...")
 
 
 # The `close_old_connections` decorator ensures that database connections, that have become
@@ -65,7 +71,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             sched_time_out_tracker_job,
-            trigger=CronTrigger(minute='30'),
+            trigger=CronTrigger(second='*/40'),
             id="sched_time_out_tracker_job",
             max_instance=1,
             replace_existing=True,
