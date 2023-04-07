903cfbf2b1147d30db144cbebe86d3ded729a551
kensidiangco <kensidiangco@gmail.com>
2023-04-08 02:44:15 +0800

    Revert

--- famsSite/wsgi.py
+++ famsSite/wsgi.py
@@ -1,8 +1,7 @@
 import os
-import subprocess
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'famsSite.settings')
 
 application = get_wsgi_application()
-subprocess.call(['python', 'manage.py', 'runapscheduler'])
