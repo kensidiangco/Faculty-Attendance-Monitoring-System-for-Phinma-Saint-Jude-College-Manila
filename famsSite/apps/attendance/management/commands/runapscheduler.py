8bd8e0c8a2267b547a820cd725df9d581bb496dd
kensidiangco <kensidiangco@gmail.com>
2023-04-07 20:07:03 +0800

    Update apscheduler command

--- famsSite/apps/attendance/management/commands/runapscheduler.py
+++ famsSite/apps/attendance/management/commands/runapscheduler.py
@@ -42,14 +42,14 @@ def sched_time_out_tracker_job():
 # unusable or are obsolete, are closed before and after your job has run. You should use it
 # to wrap any jobs that you schedule that access the Django database in any way. 
 @util.close_old_connections
-def delete_old_job_executions(max_age=604_800):
+def delete_old_job_executions(max_age=43_200):
     """
     This job deletes APScheduler job execution entries older than `max_age` from the database.
     It helps to prevent the database from filling up with old historical records that are no
     longer useful.
 
     :param max_age: The maximum length of time to retain historical job execution records.
-                    Defaults to 7 days.
+                    Defaults to half day.
     """
     DjangoJobExecution.objects.delete_old_job_executions(max_age)
 
@@ -71,21 +71,23 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             sched_time_out_tracker_job,
-            trigger=CronTrigger(second='*/40'),
+            trigger=CronTrigger(minute='*/5'),
             id="sched_time_out_tracker_job",
             max_instance=1,
             replace_existing=True,
+            misfire_grace_time=64,
         )
         logger.info("Added job 'sched_time_out_tracker_job'.")
 
         scheduler.add_job(
             delete_old_job_executions,
             trigger=CronTrigger(
-                day_of_week="mon", hour="00", minute="00"
+                hour="0", minute="0"
             ),  # Midnight on Monday, before start of the next work week.
             id="delete_old_job_executions",
             max_instances=1,
             replace_existing=True,
+            misfire_grace_time=3600,
         )
         logger.info(
             "Added weekly job: 'delete_old_job_executions'."
