3a075d50a7c5f2295ac91229828b9b813f21f0cb
kensidiangco <kensidiangco@gmail.com>
2023-04-09 18:53:31 +0800

    Fix unique job id

--- jobs/updater.py
+++ jobs/updater.py
@@ -1,5 +1,6 @@
 import logging
 import pytz
+import uuid
 from django.conf import settings
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
@@ -11,12 +12,13 @@ timezone = pytz.timezone('Asia/Manila')
 
 scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
 scheduler.add_jobstore(DjangoJobStore(), "default")
+job_id = str(uuid.uuid4()) # generate a UUID for the job ID
 
 def start_jobs():
     scheduler.add_job(
         update_sched_job,
         trigger=CronTrigger(hour='0', minute='0'),  # Every midnight
-        id="update_sched_job",  # The `id` assigned to each job MUST be unique
+        id="update_sched_job_{}".format(job_id),  # The `id` assigned to each job MUST be unique
         max_instances=1,
         replace_existing=True,
     )
@@ -25,7 +27,7 @@ def start_jobs():
     scheduler.add_job(
         sched_time_out_tracker_job,
         trigger=CronTrigger(minute='*/5'), # Run every 5 mins
-        id="sched_time_out_tracker_job",
+        id="sched_time_out_tracker_job_{}".format(job_id),
         max_instance=1,
         replace_existing=True,
         misfire_grace_time=64,
@@ -37,7 +39,7 @@ def start_jobs():
         trigger=CronTrigger(
             hour="0", minute="0"
         ),  # Midnight on Monday, before start of the next work week.
-        id="delete_old_job_executions",
+        id="delete_old_job_executions_{}".format(job_id),
         max_instances=1,
         replace_existing=True,
         misfire_grace_time=3600,
