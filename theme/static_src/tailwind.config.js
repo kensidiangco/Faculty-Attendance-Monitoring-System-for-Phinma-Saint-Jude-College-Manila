32ae3c322ce12f2c2640525e96a4c5046c5efb81
kensidiangco <kensidiangco@gmail.com>
2023-04-18 01:12:29 +0800

    Update tailwind

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -7,24 +7,41 @@
 
 module.exports = {
     content: [
-        // Templates within theme app (e.g. base.html)
+        /**
+         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+         */
+
+        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
         '../templates/**/*.html',
-        // Templates in other apps
+
+        /*
+         * Main templates directory of the project (BASE_DIR/templates).
+         * Adjust the following line to match your project structure.
+         */
         '../../templates/**/*.html',
-        // Ignore files in node_modules
-        '!../../**/node_modules',
-        // Include JavaScript files that might contain Tailwind CSS classes
-        '../../**/*.js',
-        // Include Python files that might contain Tailwind CSS classes
-        '../../**/*.py'
+
+        /*
+         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+         * Adjust the following line to match your project structure.
+         */
+        '../../**/templates/**/*.html',
+
+        /**
+         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+         * patterns match your project structure.
+         */
+        /* JS 1: Ignore any JavaScript in node_modules folder. */
+        // '!../../**/node_modules',
+        /* JS 2: Process all JavaScript files in the project. */
+        // '../../**/*.js',
+
+        /**
+         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+         * and make sure the pattern below matches your project structure.
+         */
+        // '../../**/*.py'
     ],
     theme: {
-        colors: {
-            'bg-sjc-green': '#52735D',
-            'bg-sjc-item-green': '#355C41',
-            'bg-sjc-th-green': '#52735D',
-        },
-
         extend: {},
     },
     plugins: [
